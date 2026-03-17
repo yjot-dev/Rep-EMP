3b5114783b48d6db1586aa402b9d92b910adb376
yjot <2010guabo@gmail.com>
2026-03-17 16:41:15 -0500

    Mejoras del código

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.yjotdev.empprimaria"
         minSdk = 24
         targetSdk = 35
-        versionCode = 4
-        versionName = "3.4"
+        versionCode = 5
+        versionName = "1.5"
         testInstrumentationRunner = "com.yjotdev.empprimaria.CustomTestRunner"
         vectorDrawables {
             useSupportLibrary = true
