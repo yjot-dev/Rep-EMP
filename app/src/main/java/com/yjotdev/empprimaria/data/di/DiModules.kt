0232dd8922c78f3dc780b179b838f3c7b297ec6a
yjot2 <2010guabo@gmail.com>
2026-05-16 18:51:28 -0500

    Ajustes en DiModules.kt

--- app/src/main/java/com/yjotdev/empprimaria/data/di/DiModules.kt
+++ app/src/main/java/com/yjotdev/empprimaria/data/di/DiModules.kt
@@ -1,21 +1,21 @@
 package com.yjotdev.empprimaria.data.di
 
-import dagger.Module
-import dagger.Binds
-import dagger.hilt.InstallIn
-import dagger.hilt.components.SingletonComponent
-import dagger.Provides
-import javax.inject.Singleton
-import retrofit2.Retrofit
-import com.yjotdev.empprimaria.data.remote.network.RetrofitBuilder
-import com.yjotdev.empprimaria.data.remote.api.UserApi
 import com.yjotdev.empprimaria.data.remote.api.EmailApi
+import com.yjotdev.empprimaria.data.remote.api.UserApi
+import com.yjotdev.empprimaria.data.remote.network.RetrofitBuilder
 import com.yjotdev.empprimaria.data.repository.EmailRepositoryImpl
 import com.yjotdev.empprimaria.data.repository.StringRepositoryImpl
 import com.yjotdev.empprimaria.data.repository.UserRepositoryImpl
 import com.yjotdev.empprimaria.domain.repository.EmailRepository
 import com.yjotdev.empprimaria.domain.repository.StringRepository
 import com.yjotdev.empprimaria.domain.repository.UserRepository
+import dagger.Binds
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -51,13 +51,13 @@ abstract class DiModules {
 
         @Provides
         @Singleton
-        fun provideUserService(retrofit: Retrofit): UserApi {
+        fun provideUserApi(retrofit: Retrofit): UserApi {
             return retrofit.create(UserApi::class.java)
         }
 
         @Provides
         @Singleton
-        fun provideEmailService(retrofit: Retrofit): EmailApi {
+        fun provideEmailApi(retrofit: Retrofit): EmailApi {
             return retrofit.create(EmailApi::class.java)
         }
     }
