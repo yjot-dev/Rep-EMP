2cb8a6a08e63e21a4bf4395d55df979fbeec77b5
yjot2 <2010guabo@gmail.com>
2026-06-06 15:31:54 -0500

    Subida de version de codigo

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.yjotdev.empprimaria"
         minSdk = 24
         targetSdk = 36
-        versionCode = 7
-        versionName = "1.7"
+        versionCode = 8
+        versionName = "1.8"
         testInstrumentationRunner = "com.yjotdev.empprimaria.CustomTestRunner"
         vectorDrawables {
             useSupportLibrary = true
